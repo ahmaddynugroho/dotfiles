14910532a47fa253ae75910bd023158d5b5a6d91
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2022-04-28 20:36:14 +0700

    feat: auto install packer

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,13 @@
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+end
+
+local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
+vim.api.nvim_create_autocmd('BufWritePost', { command = 'source <afile> | PackerCompile', group = packer_group, pattern = 'init.lua' })
+
 return require('packer').startup(function(use)
   -- SPEEDUP NEOVIM
   use 'lewis6991/impatient.nvim'
