91ef1b3891f985e13e74125ee4d2425e13fdcf10
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2022-05-28 12:37:18 +0700

    feat: enable TS highlight

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,6 +40,7 @@ return require("packer").startup(function(use)
 			require("nvim-treesitter.configs").setup({
 				ensure_installed = { "lua", "typescript", "javascript", "php" },
 				sync_install = true,
+				highlight = { enable = true, additional_vim_regex_highlighting = false },
 				rainbow = {
 					enable = true,
 					extended_mode = true, -- Also highlight non-bracket delimiters like html tags, boolean or table: lang -> boolean
