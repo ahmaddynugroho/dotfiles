00683ac25986b662007c912263ad8ccbe34147b0
ahmaddynugroho <ahmaddwi700@gmail.com>
2022-05-31 10:58:04 +0700

    feat: tokyonight transparent

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -1,6 +1,7 @@
 vim.opt.termguicolors = true
-vim.g.tokyonight_style = 'night'
-vim.cmd[[colorscheme tokyonight]]
+vim.g.tokyonight_style = "night"
+vim.g.tokyonight_transparent = true
+vim.cmd([[colorscheme tokyonight]])
 
 vim.opt.mouse = "a"
 vim.opt.number = true
@@ -12,11 +13,11 @@ vim.opt.expandtab = true
 vim.opt.ignorecase = true
 
 -- coc
-vim.cmd[[
+vim.cmd([[
 	set hidden
 	set nobackup
 	set nowritebackup
 	set cmdheight=2
 	set updatetime=300
 	set shortmess+=c
-]]
+]])
