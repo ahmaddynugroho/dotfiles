0afe5c3741c604e58b247aa9b9bad37c2357f034
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2022-08-30 22:38:54 +0700

    feat: autoread

--- nvim/lua/base.lua
+++ nvim/lua/base.lua
@@ -3,7 +3,12 @@ vim.g.context_enabled = 0
 vim.opt.termguicolors = true
 vim.g.tokyonight_style = "night"
 vim.g.tokyonight_transparent = true
-vim.cmd([[colorscheme tokyonight]])
+vim.cmd("colorscheme tokyonight")
+
+-- Autoread
+vim.cmd("set autoread")
+vim.cmd("au CursorHold,CursorHoldI * checktime")
+vim.cmd("au FocusGained,BufEnter * :checktime")
 
 vim.opt.shortmess = "I"
 vim.opt.mouse = "a"
@@ -18,4 +23,4 @@ vim.opt.autoindent = true
 vim.opt.undofile = true
 vim.opt.lazyredraw = true
 
-vim.cmd([[autocmd FileType php setlocal autoindent]])
+vim.cmd("autocmd FileType php setlocal autoindent")
