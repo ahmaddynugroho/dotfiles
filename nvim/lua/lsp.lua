706f1ec8013f83e58340d4b1b784ff51b7009a41
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2022-11-28 16:12:46 +0700

    chore(lsp): add rust_analyzer and some clean up

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,32 +1,38 @@
-local l = require("lspconfig")
-local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
-
-l.sumneko_lua.setup({
-	capabilities = capabilities,
-	on_attach = Keybinding.lsp_on_attach,
-	settings = {
-		Lua = {
-			runtime = { version = "LuaJIT" },
-			diagnostics = { globals = { "vim" } },
-			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-			telemetry = { enable = false },
-		},
-	},
-})
-l.rescriptls.setup({
-	capabilities = capabilities,
-	on_attach = Keybinding.lsp_on_attach,
-	cmd = {
-		"node",
-		"/home/civbag/.local/share/nvim/site/pack/packer/start/vim-rescript/server/out/server.js",
-		"--stdio",
-	},
-})
-
-local servers = { "intelephense", "tsserver", "emmet_ls", "zls", "pyright", "nimls" }
+local servers =
+	{ "sumneko_lua", "rescriptls", "intelephense", "tsserver", "emmet_ls", "zls", "pyright", "nimls", "rust_analyzer" }
 for _, lsp in pairs(servers) do
-	require("lspconfig")[lsp].setup({
-		capabilities = capabilities,
+	local sttn = { -- default settings
+		capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
 		on_attach = Keybinding.lsp_on_attach,
-	})
+		settings = {}, -- empty table to remove warning
+	}
+	local spcf = { -- specific settings
+		sumneko_lua = {
+			settings = {
+				Lua = {
+					runtime = { version = "LuaJIT" },
+					diagnostics = { globals = { "vim" } },
+					workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+					telemetry = { enable = false },
+				},
+			},
+		},
+		rescriptls = {
+			cmd = {
+				"node",
+				"/home/civbag/.local/share/nvim/site/pack/packer/start/vim-rescript/server/out/server.js",
+				"--stdio",
+			},
+		},
+	}
+	for k, v in pairs(spcf) do
+		if lsp ~= k then
+			break
+		end
+		for j, w in pairs(v) do
+			sttn[j] = w
+		end
+	end
+
+	require("lspconfig")[lsp].setup(sttn)
 end
