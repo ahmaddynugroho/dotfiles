9f58bcf53a9abe98f0112bea6a75cc4493de1114
Ahmad D. Y. Nugroho <ahmaddwi700@gmail.com>
2022-11-30 19:26:25 +0700

    fix(nvim): fix lsp settings not included

--- nvim/lua/lsp.lua
+++ nvim/lua/lsp.lua
@@ -1,40 +1,32 @@
-local servers =
-	{ "sumneko_lua", "rescriptls", "intelephense", "tsserver", "emmet_ls", "zls", "pyright", "nimls", "rust_analyzer" }
---[[ local servers =
-	{ "sumneko_lua", "intelephense" } ]]
-for _, lsp in pairs(servers) do
-	local sttn = { -- default settings
-		capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities()),
-		on_attach = Keybinding.lsp_on_attach,
-	}
-	local spcf = { -- specific settings
-		sumneko_lua = {
-			settings = {
-				Lua = {
-					runtime = { version = "LuaJIT" },
-					diagnostics = { globals = { "vim" } },
-					workspace = { library = vim.api.nvim_get_runtime_file("", true) },
-					telemetry = { enable = false },
-				},
-			},
-		},
-		rescriptls = {
-			cmd = {
-				"node",
-				"/home/civbag/.local/share/nvim/site/pack/packer/start/vim-rescript/server/out/server.js",
-				"--stdio",
-			},
-		},
-	}
-	for k, v in pairs(spcf) do
-		if lsp ~= k then
-			break
-		end
-		for j, w in pairs(v) do
-			sttn[j] = w
-		end
-	end
-	-- vim.pretty_print(sttn)
+local l = require("lspconfig")
+local capabilities = require("cmp_nvim_lsp").default_capabilities(vim.lsp.protocol.make_client_capabilities())
 
-	require("lspconfig")[lsp].setup(sttn)
+l["sumneko_lua"].setup({
+	capabilities = capabilities,
+	on_attach = Keybinding.lsp_on_attach,
+	settings = {
+		Lua = {
+			runtime = { version = "LuaJIT" },
+			diagnostics = { globals = { "vim" } },
+			workspace = { library = vim.api.nvim_get_runtime_file("", true) },
+			telemetry = { enable = false },
+		},
+	},
+})
+l["rescriptls"].setup({
+	capabilities = capabilities,
+	on_attach = Keybinding.lsp_on_attach,
+	cmd = {
+		"node",
+		"/home/civbag/.local/share/nvim/site/pack/packer/start/vim-rescript/server/out/server.js",
+		"--stdio",
+	},
+})
+
+local servers = { "intelephense", "tsserver", "emmet_ls", "zls", "pyright", "nimls", "rust_analyzer" }
+for _, lsp in pairs(servers) do
+	l[lsp].setup({
+		capabilities = capabilities,
+		on_attach = Keybinding.lsp_on_attach,
+	})
 end
