c13eeaea5298682e456f452128326527fe343a13
ahmaddynugroho <ahmaddwi700@gmail.com>
2024-08-26 19:49:17 +0700

    tilde remap, wez linux config

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,22 +11,41 @@ if wezterm.config_builder then
 	config = wezterm.config_builder()
 end
 
+-- https://github.com/wez/wezterm/discussions/4728
+local is_windows = wezterm.target_triple:find("windows") ~= nil
+local is_linux = wezterm.target_triple:find("linux") ~= nil
+
 -- This is where you actually apply your config choices
 
 -- For example, changing the color scheme:
-config.color_scheme = "catppuccin-mocha"
-config.default_prog = { "wsl", "--cd", "~" }
+config.color_scheme = "rose-pine"
 config.font = wezterm.font("BlexMono Nerd Font")
-config.font_size = 8.5
 config.hide_tab_bar_if_only_one_tab = true
-config.background = {
-	{
-		source = {
-			File = "C:/Users/Yara/Pictures/Wallpaper/study club.jpeg",
+
+if is_windows then
+	config.font_size = 8.5
+	config.default_prog = { "wsl", "--cd", "~" }
+	config.background = {
+		{
+			source = {
+				File = "C:/Users/Yara/Pictures/Wallpaper/study club.jpeg",
+			},
+			hsb = { brightness = 0.02 },
 		},
-		hsb = { brightness = 0.02 },
-	},
-}
+	}
+end
+
+if is_linux then
+	config.font_size = 13
+	config.background = {
+		{
+			source = {
+				File = "/home/miku4j/Pictures/arius.png",
+			},
+			hsb = { brightness = 0.015 },
+		},
+	}
+end
 
 wezterm.on("gui-startup", function(cmd)
 	local _, _, window = mux.spawn_window(cmd or {})
